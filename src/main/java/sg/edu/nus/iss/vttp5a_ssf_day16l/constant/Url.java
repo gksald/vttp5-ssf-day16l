7035f48f76e5e711513e3fa1ca3a6f8f1affd7f6
darryl1975 <darryl1975@hotmail.com>
2024-11-28 16:47:33 +0800

    added openapi from api.first.org

--- src/main/java/sg/edu/nus/iss/vttp5a_ssf_day16l/constant/Url.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_day16l/constant/Url.java
@@ -3,4 +3,6 @@ package sg.edu.nus.iss.vttp5a_ssf_day16l.constant;
 public class Url {
     
     public static final String studentUrl = "http://localhost:3000/api/students";
+
+    public static final String countryUrl = "https://api.first.org/data/v1/countries";
 }
